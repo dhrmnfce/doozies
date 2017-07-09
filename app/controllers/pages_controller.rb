eeaa4aed9e68073eabb1d302b691cf5821181023
dhrmnfce <dylanludwick@gmail.com>
2017-07-09 18:38:43 +0000

    Add basic SEO

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
     
     def index
-    
+        @meta_title = "Doozie's Homemade Ice Cream & Burgers"
+        @meta_description = "Doozie's is serving up delicious homemade ice cream and juicy burgers. Located across the street from the Council Bluffs Country Club."
     end
     
     def menu
-        
+        @meta_title = "Doozie's Homemade Ice Cream & Burgers Menu"
+        @meta_description = "Our menu has everything from juicy burgers to crispy tenderloins and salads - from our homemade ice cream to classic shakes & malts. We have enough for you to always try something new!"
     end
     
 end
